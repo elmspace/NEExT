ba87479c07de809c7fb72c1c4e5e0add4a01ecd8
Ashkan Dehghan <ash@Ashkans-MacBook-Air.local>
2023-08-31 19:20:34 +0000

    Adding tqdm disable flag

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from ugaf.ugaf import UGAF
 
 
 config = {}
-config["g1_path"] = "/Users/ash/Desktop/development/research/testing/ugaf/mix_1.csv"
-config["g2_path"] = "/Users/ash/Desktop/development/research/testing/ugaf/mix_2.csv"
+config["g1_path"] = "/Users/ash/Desktop/development/research/testing/ugaf/data/basic_random_graphs/lsme/g_0.csv"
+config["g2_path"] = "/Users/ash/Desktop/development/research/testing/ugaf/data/basic_random_graphs/lsme/g_0.csv"
 config["sample_rate"] = 0.05
 config["ensemble_size"] = 10
 
--- ugaf/ugaf.py
+++ ugaf/ugaf.py
@@ -18,6 +18,9 @@ class UGAF:
 
 	def __init__(self, config):
 		self.config = config
+		self.tqdm_disable = True
+		if "tqdm_disable" in self.config:
+			self.tqdm_disable = self.config["tqdm_disable"]
 		self.g1_emb = pd.DataFrame()
 		self.g2_emb = pd.DataFrame()
 		self.emb_samps = {"g1":[], "g2":[]}
@@ -52,7 +55,7 @@ class UGAF:
 			This method samples the two graph embedding based on the sample rate defined by the user.
 		"""
 		sample_size = int(self.config["sample_rate"] * min([len(self.g1_emb), len(self.g2_emb)]))
-		for i in tqdm(range(self.config["ensemble_size"]), desc="Sampling Embeddings:"):
+		for i in tqdm(range(self.config["ensemble_size"]), desc="Sampling Embeddings:", disable=self.tqdm_disable):
 			self.emb_samps["g1"].append(self.g1_emb.sample(n=sample_size))
 			self.emb_samps["g2"].append(self.g2_emb.sample(n=sample_size))
 
@@ -63,21 +66,21 @@ class UGAF:
 			graph embeddings.
 		"""
 		# g1 sims
-		for i in tqdm(range(len(self.emb_samps["g1"])), desc="Similarity Measure g1vg1"):
+		for i in tqdm(range(len(self.emb_samps["g1"])), desc="Similarity Measure g1vg1", disable=self.tqdm_disable):
 			for j in range(i+1, len(self.emb_samps["g1"])):
 				e1 = self.emb_samps["g1"][i]
 				e2 = self.emb_samps["g1"][j]
 				self.sim_matrices["g1vg1"].append(cosine_similarity(e1.values, e2.values))
 		
 		# g2 sims
-		for i in tqdm(range(len(self.emb_samps["g2"])-1), desc="Similarity Measure g2vg2"):
+		for i in tqdm(range(len(self.emb_samps["g2"])-1), desc="Similarity Measure g2vg2", disable=self.tqdm_disable):
 			for j in range(i+1, len(self.emb_samps["g2"])):
 				e1 = self.emb_samps["g2"][i]
 				e2 = self.emb_samps["g2"][j]
 				self.sim_matrices["g2vg2"].append(cosine_similarity(e1.values, e2.values))
 		
 		# g1 v g2 sims
-		for i in tqdm(range(len(self.emb_samps["g1"])), desc="Similarity Measure g1vg2"):
+		for i in tqdm(range(len(self.emb_samps["g1"])), desc="Similarity Measure g1vg2", disable=self.tqdm_disable):
 			for j in range(i, len(self.emb_samps["g2"])):
 				e1 = self.emb_samps["g1"][i]
 				e2 = self.emb_samps["g2"][j]
@@ -91,7 +94,7 @@ class UGAF:
 		"""
 		# G1vG1 Measure
 		was_dist_g1vg1 = []
-		for i in tqdm(range(len(self.sim_matrices["g1vg1"])), desc="Measuring Wasserstein Distance g1vg1"):
+		for i in tqdm(range(len(self.sim_matrices["g1vg1"])), desc="Measuring Wasserstein Distance g1vg1", disable=self.tqdm_disable):
 			for j in range(i+1, len(self.sim_matrices["g1vg1"])):
 				s1 = self.sim_matrices["g1vg1"][i].flatten()
 				s2 = self.sim_matrices["g1vg1"][j].flatten()
@@ -100,7 +103,7 @@ class UGAF:
 		was_dist_g1vg1 = np.array(was_dist_g1vg1)
 		# G2vG2 Measure
 		was_dist_g2vg2 = []
-		for i in tqdm(range(len(self.sim_matrices["g2vg2"])-1), desc="Measuring Wasserstein Distance g2vg2"):
+		for i in tqdm(range(len(self.sim_matrices["g2vg2"])-1), desc="Measuring Wasserstein Distance g2vg2", disable=self.tqdm_disable):
 			for j in range(i+1, len(self.sim_matrices["g2vg2"])):
 				s1 = self.sim_matrices["g2vg2"][i].flatten()
 				s2 = self.sim_matrices["g2vg2"][j].flatten()
@@ -109,7 +112,7 @@ class UGAF:
 		was_dist_g2vg2 = np.array(was_dist_g2vg2)
 		# G1vG2 Measure
 		was_dist_g1vg2 = []
-		for i in tqdm(range(len(self.sim_matrices["g1vg2"])-1), desc="Measuring Wasserstein Distance g1vg2"):
+		for i in tqdm(range(len(self.sim_matrices["g1vg2"])-1), desc="Measuring Wasserstein Distance g1vg2", disable=self.tqdm_disable):
 			for j in range(i+1, len(self.sim_matrices["g1vg2"])):
 				s1 = self.sim_matrices["g1vg2"][i].flatten()
 				s2 = self.sim_matrices["g1vg2"][j].flatten()
@@ -133,21 +136,21 @@ class UGAF:
 		self.stats["sim_matrix_eigen_values"]["g1vg2"] = {"eigen_values":[]}
 		# Compute eigen values for g1vg1
 		eigen_g1vg1 = []
-		for i in tqdm(range(len(self.sim_matrices["g1vg1"])), desc="Eigen values of g1vg1"):
+		for i in tqdm(range(len(self.sim_matrices["g1vg1"])), desc="Eigen values of g1vg1", disable=self.tqdm_disable):
 			val = np.linalg.eig(self.sim_matrices["g1vg1"][i])
 			val = val[0][0].real
 			eigen_g1vg1.append(val)
 		self.stats["sim_matrix_eigen_values"]["g1vg1"]["eigen_values"] = eigen_g1vg1
 		# Compute eigen values for g2vg2
 		eigen_g2vg2 = []
-		for i in tqdm(range(len(self.sim_matrices["g2vg2"])), desc="Eigen values of g2vg2"):
+		for i in tqdm(range(len(self.sim_matrices["g2vg2"])), desc="Eigen values of g2vg2", disable=self.tqdm_disable):
 			val = np.linalg.eig(self.sim_matrices["g2vg2"][i])
 			val = val[0][0].real
 			eigen_g2vg2.append(val)
 		self.stats["sim_matrix_eigen_values"]["g2vg2"]["eigen_values"] = eigen_g2vg2
 		# Compute eigen values for g1vg2
 		eigen_g1vg2 = []
-		for i in tqdm(range(len(self.sim_matrices["g1vg2"])), desc="Eigen values of g1vg2"):
+		for i in tqdm(range(len(self.sim_matrices["g1vg2"])), desc="Eigen values of g1vg2", disable=self.tqdm_disable):
 			val = np.linalg.eig(self.sim_matrices["g1vg2"][i])
 			val = val[0][0].real
 			eigen_g1vg2.append(val)
@@ -163,7 +166,7 @@ class UGAF:
 		self.stats["sim_matrix_dist_properties"]["g2vg2"] = {"kurtosis":[], "mean":[], "std":[], "skew":[], "cov":[]}
 		self.stats["sim_matrix_dist_properties"]["g1vg2"] = {"kurtosis":[], "mean":[], "std":[], "skew":[], "cov":[]}
 		# Run stats g1vg1
-		for mtx in tqdm(self.sim_matrices["g1vg1"], desc="Computing sim-matrix properties g1vg1:"):
+		for mtx in tqdm(self.sim_matrices["g1vg1"], desc="Computing sim-matrix properties g1vg1:", disable=self.tqdm_disable):
 			mtx_flat = mtx.flatten()
 			self.stats["sim_matrix_dist_properties"]["g1vg1"]["kurtosis"].append(kurtosis(mtx_flat))
 			self.stats["sim_matrix_dist_properties"]["g1vg1"]["mean"].append(mtx_flat.mean())
@@ -171,7 +174,7 @@ class UGAF:
 			self.stats["sim_matrix_dist_properties"]["g1vg1"]["skew"].append(skew(mtx_flat))
 			self.stats["sim_matrix_dist_properties"]["g1vg1"]["cov"].append(np.std(mtx_flat)/mtx_flat.mean())
 		# Run stats g2vg2
-		for mtx in tqdm(self.sim_matrices["g2vg2"], desc="Computing sim-matrix properties g2vg2:"):
+		for mtx in tqdm(self.sim_matrices["g2vg2"], desc="Computing sim-matrix properties g2vg2:", disable=self.tqdm_disable):
 			mtx_flat = mtx.flatten()
 			self.stats["sim_matrix_dist_properties"]["g2vg2"]["kurtosis"].append(kurtosis(mtx_flat))
 			self.stats["sim_matrix_dist_properties"]["g2vg2"]["mean"].append(mtx_flat.mean())
@@ -179,7 +182,7 @@ class UGAF:
 			self.stats["sim_matrix_dist_properties"]["g2vg2"]["skew"].append(skew(mtx_flat))
 			self.stats["sim_matrix_dist_properties"]["g2vg2"]["cov"].append(np.std(mtx_flat)/mtx_flat.mean())
 		# Run stats g1vg2
-		for mtx in tqdm(self.sim_matrices["g1vg2"], desc="Computing sim-matrix properties g1vg2:"):
+		for mtx in tqdm(self.sim_matrices["g1vg2"], desc="Computing sim-matrix properties g1vg2:", disable=self.tqdm_disable):
 			mtx_flat = mtx.flatten()
 			self.stats["sim_matrix_dist_properties"]["g1vg2"]["kurtosis"].append(kurtosis(mtx_flat))
 			self.stats["sim_matrix_dist_properties"]["g1vg2"]["mean"].append(mtx_flat.mean())
