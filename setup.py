4fe1af5973a2525f2b37b0c9a2c033b3267ae384
ash <ash@pop-os.localdomain>
2023-10-18 14:39:15 +0000

    Adding umap plotting functionality.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name='ugaf',
-	version='0.0.4',
+	version='0.0.5',
 	author='Ashkan Dehghan',
 	author_email='ash.dehghan@gmail.com',
 	description='Unsupervised Graph Analysis Framework.',
@@ -27,5 +27,6 @@ setuptools.setup(
 	"vectorizers==0.2",
 	"scipy==1.11.2",
 	"numpy==1.25.2",
-	"karateclub==1.2.2"]
+	"karateclub==1.2.2",
+	"umap-learn"]
 )
--- ugaf/ugaf.py
+++ ugaf/ugaf.py
@@ -21,7 +21,7 @@ class UGAF:
 		self.graph_c = Graph_Collection()
 		self.emb_eng = Embedding_Engine()
 		self.gc_status = False
-
+		self.graph_embedding = {}
 
 	def check_gc_status(func):
 		def _check_gc_status(self, *args, **kwargs):
@@ -73,7 +73,11 @@ class UGAF:
 
 	@check_gc_status
 	def build_node_embedding(self, embedding_type, emb_dim):
-		# Run embedding calculation
+		"""
+			This function takes as input the embedding type, and dimension
+			and uses the embedding object to build embeddings for the nodes of 
+			graphs in the graph collection.
+		"""
 		logger.info("Running %s embedding" % (embedding_type))
 		for g_obj in tqdm(self.graph_c.graph_collection, desc="Building embeddings"):
 			G = g_obj["graph"]
@@ -101,7 +105,7 @@ class UGAF:
 
 
 	@check_gc_status
-	def build_graph_embedding(self, using_embedding):
+	def build_graph_embedding(self, source_embedding):
 		logger.info("Creating incidence matrix")
 		n = self.graph_c.total_numb_of_nodes
 		rows = self.graph_c.graph_id_node_array
@@ -112,9 +116,9 @@ class UGAF:
 		for g_obj in tqdm(self.graph_c.graph_collection, desc="Loading embeddings"):
 			if "embedding" not in g_obj:
 				logger.error("You have to run node/structural embedding first.")
-			if using_embedding not in g_obj["embedding"]:
+			if source_embedding not in g_obj["embedding"]:
 				logger.error("No such selected embedding.")
-			embs = g_obj["embedding"][using_embedding]
+			embs = g_obj["embedding"][source_embedding]
 			embedding_collection.append(list(embs.values()))
 
 		embedding_collection = np.array(embedding_collection, dtype=object)
@@ -124,5 +128,18 @@ class UGAF:
 			normalization_power=0.66,
 			random_state=42,
 		).fit_transform(incidence_matrix, vectors=embedding_collection)
+		self.graph_embedding[source_embedding] = graphs_embed
 		
+
+	@check_gc_status
+	def run_umap(self, source_embedding, plot=False):
+		"""
+			This function uses the graph embeddings built using source node/structural embedding
+			and saves it, and also plots it, if <plot> flag is set to True.
+		"""
+		# Check if source embedding exists
+		if not source_embedding in self.graph_embedding:
+			logger.error("Missing graph embedding for source: %s" %(source_embedding))
+			exit(0)
+
 		
