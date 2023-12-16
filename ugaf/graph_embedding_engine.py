f752ddd7f0f9984cf70738a4e49a17a9f8c7dbbb
Ash D <ash.dehghan@gmail.com>
2023-12-16 19:02:50 +0000

    Adding two new graph embedding types

--- ugaf/graph_embedding_engine.py
+++ ugaf/graph_embedding_engine.py
@@ -24,14 +24,18 @@ class Graph_Embedding_Engine:
 
 		graph_embedding_type = self.global_config.config["graph_embedding"]["embedding_type"]
 
-		if graph_embedding_type == "wasserstein":
+		if graph_embedding_type == "approx_wasserstein":
+			graphs_embed, graph_embedding_df = self.build_approx_wasserstein_graph_embedding(graph_c)
+		elif graph_embedding_type == "wasserstein":
 			graphs_embed, graph_embedding_df = self.build_wasserstein_graph_embedding(graph_c)
+		elif graph_embedding_type == "sinkhornvectorizer":
+			graphs_embed, graph_embedding_df = self.build_sinkhornvectorizer_graph_embedding(graph_c)
 		else:
 			raise ValueError("Graph embedding type selected is not supported.")
 		return graphs_embed, graph_embedding_df
 
 
-	def build_wasserstein_graph_embedding(self, graph_c):
+	def build_approx_wasserstein_graph_embedding(self, graph_c):
 		"""
 			This method uses the source node mebdding type and builds the graph
 			embedding using the Wasserstein method.
@@ -60,6 +64,65 @@ class Graph_Embedding_Engine:
 		graph_embedding_df["graph_id"] = graph_ids
 		return graphs_embed, graph_embedding_df
 
+
+	def build_wasserstein_graph_embedding(self, graph_c):
+		"""
+			This method uses the source node mebdding type and builds the graph
+			embedding using the Wasserstein method.
+			** Note this method does not make sense for classical node embeddings.
+		"""
+		n = graph_c.total_numb_of_nodes
+		rows = graph_c.graph_id_node_array
+		cols = np.arange(n)
+		incidence_matrix = scipy.sparse.csr_matrix((np.repeat(1.0,n).astype(np.float32), (rows, cols)))
+		embedding_collection = graph_c.global_embeddings[graph_c.global_embeddings_cols].values
+		if self.global_config.config["graph_embedding"]["graph_emb_dim"] == "auto":
+			graph_emb_dim = graph_c.global_embeddings[graph_c.global_embeddings_cols].shape[1]
+		else:
+			graph_emb_dim = self.global_config.config["graph_embedding"]["graph_emb_dim"]
+		graph_ids = graph_c.global_embeddings["graph_id"].unique().tolist()
+		embedding_collection = np.array(embedding_collection, dtype=object)
+		embedding_collection = np.vstack(embedding_collection)
+		graphs_embed = vectorizers.WassersteinVectorizer(
+			memory_size="4G",
+			random_state=42,
+			n_components=graph_emb_dim
+		).fit_transform(incidence_matrix.astype(float), vectors=embedding_collection.astype(float))
+		graph_embedding_df = pd.DataFrame(graphs_embed)
+		emb_cols = ["emb_"+str(i) for i in range(graph_embedding_df.shape[1])]
+		graph_embedding_df.columns = emb_cols
+		graph_embedding_df["graph_id"] = graph_ids
+		return graphs_embed, graph_embedding_df
+
+
+	def build_sinkhornvectorizer_graph_embedding(self, graph_c):
+		"""
+			This method uses the source node mebdding type and builds the graph
+			embedding using the Wasserstein method.
+			** Note this method does not make sense for classical node embeddings.
+		"""
+		n = graph_c.total_numb_of_nodes
+		rows = graph_c.graph_id_node_array
+		cols = np.arange(n)
+		incidence_matrix = scipy.sparse.csr_matrix((np.repeat(1.0,n).astype(np.float32), (rows, cols)))
+		embedding_collection = graph_c.global_embeddings[graph_c.global_embeddings_cols].values
+		if self.global_config.config["graph_embedding"]["graph_emb_dim"] == "auto":
+			graph_emb_dim = graph_c.global_embeddings[graph_c.global_embeddings_cols].shape[1]
+		else:
+			graph_emb_dim = self.global_config.config["graph_embedding"]["graph_emb_dim"]
+		graph_ids = graph_c.global_embeddings["graph_id"].unique().tolist()
+		embedding_collection = np.array(embedding_collection, dtype=object)
+		embedding_collection = np.vstack(embedding_collection)
+		graphs_embed = vectorizers.SinkhornVectorizer(
+			memory_size="4G",
+			random_state=42,
+			n_components=graph_emb_dim
+		).fit_transform(incidence_matrix.astype(float), vectors=embedding_collection.astype(float))
+		graph_embedding_df = pd.DataFrame(graphs_embed)
+		emb_cols = ["emb_"+str(i) for i in range(graph_embedding_df.shape[1])]
+		graph_embedding_df.columns = emb_cols
+		graph_embedding_df["graph_id"] = graph_ids
+		return graphs_embed, graph_embedding_df
 
 # se_vectorizer = vectorizers.SinkhornVectorizer(
 #     n_components=160,
